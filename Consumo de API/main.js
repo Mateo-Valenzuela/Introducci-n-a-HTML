a0fde3b0618d37d1a908c1db2a822b55e4519fd8
Mateo-Valenzuela <matep.d35@gmail.com>
2024-06-10 20:12:56 -0300

    html5

--- Consumo de API/main.js
+++ Consumo de API/main.js
@@ -1,11 +1,15 @@
-async function solicitar(){
+const axios = require('axios');
 
-    const lista = await fetch("https://dolarapi.com/v1/dolares")
- 
-    //convertir los datos recibidos en una expresion nativa de javascript
-    const lista_definitiva = await lista.json()
- 
-    lista_definitiva.forEach((element,index) => {
-        console.log(``)
-    });
- }
+axios.get('https://dolarapi.com/v1/dolares')
+  .then(response => {
+    const data = response.data;
+    console.log(`Dólar Oficial: ${data.dolarOficial}`);
+    console.log(`Dólar Blue: ${data.dolarBlue}`);
+    console.log(`Dólar Bolsa: ${data.dolarBolsa}`);
+    console.log(`Dólar CCL: ${data.dolarCCL}`);
+    console.log(`Dólar Mayorista: ${data.dolarMayorista}`);
+    console.log(`Dólar Paralelo: ${data.dolarParalelo}`);
+  })
+  .catch(error => {
+    console.error(error);
+  });
