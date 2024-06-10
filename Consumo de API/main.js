904e29bf7ed70b9546ac7a8d18d8769964ad9e17
Mateo-Valenzuela <matep.d35@gmail.com>
2024-06-10 20:21:38 -0300

    html5

--- Consumo de API/main.js
+++ Consumo de API/main.js
@@ -1,15 +1,20 @@
-const axios = require('axios');
+async function solicitar(){
 
-axios.get('https://dolarapi.com/v1/dolares')
-  .then(response => {
-    const data = response.data;
-    console.log(`Dólar Oficial: ${data.dolarOficial}`);
-    console.log(`Dólar Blue: ${data.dolarBlue}`);
-    console.log(`Dólar Bolsa: ${data.dolarBolsa}`);
-    console.log(`Dólar CCL: ${data.dolarCCL}`);
-    console.log(`Dólar Mayorista: ${data.dolarMayorista}`);
-    console.log(`Dólar Paralelo: ${data.dolarParalelo}`);
-  })
-  .catch(error => {
-    console.error(error);
+axios.get('ttps://dolarapi.com/v1/dolares')
+const lista = await fetch("")
+then(response => {
+  const data = response.data;
+  console.log(`Dólar Oficial: ${data.dolarOficial}`);
+  console.log(`Dólar Blue: ${data.dolarBlue}`);
+  console.log(`Dólar Bolsa: ${data.dolarBolsa}`);
+  console.log(`Dólar CCL: ${data.dolarCCL}`);
+  console.log(`Dólar Mayorista: ${data.dolarMayorista}`);
+  console.log(`Dólar Paralelo: ${data.dolarParalelo}`);
+})
+  //convertir los datos recibidos en una expresion nativa de javascript
+  const lista_definitiva = await lista.json()
+
+  lista_definitiva.forEach((element,index) => {
+      console.log(`response.data`)
   });
+}
